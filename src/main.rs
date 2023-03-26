f247774bef19a530cc0385ba19f1a9d46b5129e1
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-03-26 16:44:28 -0300

    feat: start the CLI with a -k/--api-key flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+//! A command-line interface for ChatGPT.
+
+use clap::Parser;
+
+/// A command-line interface for ChatGPT.
+#[derive(Parser)]
+#[command(version, author, about)]
+struct Cli {
+    /// Your OpenAI API key.
+    #[arg(short = 'k', long)]
+    api_key: String,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     println!("Hello, world!");
 }
