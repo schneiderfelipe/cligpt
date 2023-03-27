40a5b4420a9a5c7f9c383e8bd7db172a74b0da87
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-03-27 14:46:09 -0300

    docs(README.md): update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-//! `cligpt` is a command-line interface for interacting with the ChatGPT API from OpenAI.
+//! `cligpt` is a command-line interface for interacting with the `ChatGPT` API from `OpenAI`.
 //! With `cligpt`,
-//! you can quickly and easily generate text by sending prompts to ChatGPT from your terminal.
-//! Simply provide your OpenAI API key and the message you want to generate,
+//! you can quickly and easily generate text by sending prompts to `ChatGPT` from your terminal.
+//! Simply provide your `OpenAI` API key and the message you want to generate,
 //! and `cligpt` will handle the rest.
 //!
-//! `cligpt` is written in Rust and uses the clap and `cligpt` crates to provide a user-friendly interface for the ChatGPT API.
-//! It's perfect for developers who want to quickly prototype text generation applications or for anyone who wants to experiment with the power of the ChatGPT API from the comfort of their terminal.
+//! `cligpt` is written in Rust and uses the clap and `cligpt` crates to provide a user-friendly interface for the `ChatGPT` API.
+//! It's perfect for developers who want to quickly prototype text generation applications or for anyone who wants to experiment with the power of the `ChatGPT` API from the comfort of their terminal.
 //!
 //! # Installation
 //!
@@ -34,7 +34,7 @@
 //! # Usage
 //!
 //! To use `cligpt`,
-//! you need to provide your [OpenAI API key](https://platform.openai.com/account/api-keys) and the message you want to generate.
+//! you need to provide your [`OpenAI` API key](https://platform.openai.com/account/api-keys) and the message you want to generate.
 //! You can provide the API key using the `-k` or `--api-key` option,
 //! or by setting the `OPENAI_API_KEY` environment variable.
 //!
@@ -44,7 +44,7 @@
 //! cligpt --api-key YOUR_API_KEY 'Hello, ChatGPT!'
 //! ```
 //!
-//! This will send the message "Hello, ChatGPT!" to the ChatGPT API using your API key and print the generated text to your terminal.
+//! This will send the message "Hello, `ChatGPT`!" to the `ChatGPT` API using your API key and print the generated text to your terminal.
 
 use std::error::Error;
 
