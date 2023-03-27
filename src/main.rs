ebd4bce33981debbca4b1bd01455d4bff5fcd01c
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-03-27 12:39:55 -0300

    feat: make work 🦾

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 //! A command-line interface for ChatGPT.
 
+use std::error::Error;
+
+use async_openai::{
+    types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
+    Client,
+};
 use clap::Parser;
+use futures_util::StreamExt;
 
 /// A command-line interface for ChatGPT.
 #[derive(Debug, Parser)]
@@ -14,8 +21,35 @@ struct Cli {
     message: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
+    let message = cli.message;
+    let api_key = cli.api_key;
 
-    println!("{cli:#?}");
+    let client = Client::new().with_api_key(api_key);
+    let request = CreateChatCompletionRequestArgs::default()
+        .model("gpt-3.5-turbo")
+        .max_tokens(1024u16)
+        .temperature(0.7)
+        .messages([ChatCompletionRequestMessageArgs::default()
+            .content(message)
+            .build()?])
+        .build()?;
+    let mut stream = client.chat().create_stream(request).await?;
+
+    while let Some(result) = stream.next().await {
+        match result {
+            Ok(response) => {
+                if let Some(choice) = response.choices.get(0) {
+                    if let Some(text) = &choice.delta.content {
+                        println!("{}", text);
+                    }
+                }
+            }
+            Err(error) => eprintln!("error: {}", error),
+        }
+    }
+
+    Ok(())
 }
